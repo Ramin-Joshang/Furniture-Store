1a824a806061c145623bb8b60e3cbeded7fa972a
Ramin Joshang <rjwshng@gmail.com>
2021-12-01 21:41:40 +0330

    Site header created

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// navbar searchBox
+const searchInput = document.querySelector(".search-input");
+const searchBtn = document.querySelector(".search-btn");
+
+searchBtn.addEventListener("click", () => {
+    searchInput.value = "";
+    searchInput.focus();
+});
